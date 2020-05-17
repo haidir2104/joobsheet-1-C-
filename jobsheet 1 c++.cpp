e8a14ab9f48f865a59374e598b339663f539627f
unknown <Haidirbinsef@gmail.com>
2020-05-17 23:32:00 +0800

    Jobbsheet 1 Cpp

---
+++ jobsheet 1 c++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+main (){
+	cout<<"\tNama :haidir"<<endl;
+	cout<<"\tkelompok : 12 "<<endl;
+	
+	
+string my_array[9]={"F1B019059","F1B019057","F1B019033","F1B019060","F1B019069","F1B0190xx","F1B0190xx","F1B0190xx","F1B0180xx"};
+	
+	cout<<"nim mahasiswa ke- 1 = "<<my_array[0]<<"\n";
+	cout<<"nim mahasiswa ke- 2 = "<<my_array[1]<<"\n";
+	cout<<"nim mahasiswa ke- 3 = "<<my_array[2]<<"\n";
+	cout<<"nim mahasiswa ke- 4 = "<<my_array[3]<<"\n";
+	cout<<"nim mahasiswa ke- 5 = "<<my_array[4]<<"\n";
+	cout<<"nim mahasiswa ke- 6 = "<<my_array[5]<<"\n";
+	cout<<"nim mahasiswa ke- 7 = "<<my_array[6]<<"\n";
+	cout<<"nim mahasiswa ke- 8 = "<<my_array[7]<<"\n";
+	cout<<"nim mahasiswa ke- 9 = "<<my_array[8]<<"\n";
+}
+
